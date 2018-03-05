e97e1f760cec7d37fa3ededb511c06d6eb76122b
Hearty Oh <heartyoh@hatiolab.com>
2018-03-05 18:55:40 +0900

    fix component template spec.

--- templates/legend.js
+++ templates/legend.js
@@ -1,24 +1,19 @@
+import icon from '../assets/legend.png';
+
 export default {
-  name: 'legend',
-    /* 다국어 키 표현을 어떻게.. */
-  description: '...',
-  /* 다국어 키 표현을 어떻게.. */
-  group: 'warehouse',
-  /* line|shape|textAndMedia|chartAndGauge|table|container|dataSource|IoT|3D|warehouse|form|etc */
-  icon: '../',
-  /* 또는, Object */
-  template: {
+  type: 'legend',
+  description: 'legend for visualizer',
+  group: 'warehouse', /* line|shape|textAndMedia|chartAndGauge|table|container|dataSource|IoT|3D|warehouse|form|etc */
+  icon,
+  model: {
     type: 'legend',
-    model: {
-      type: 'legend',
-      left: 100,
-      top: 100,
-      width: 200,
-      height: 150,
-      fillStyle: '#efefef',
-      direction: 'vertical',
-      strokeStyle: 'rgba(0, 0, 0, 0.3)',
-      lineWidth: 1
-    }
+    left: 100,
+    top: 100,
+    width: 200,
+    height: 150,
+    fillStyle: '#efefef',
+    direction: 'vertical',
+    strokeStyle: 'rgba(0, 0, 0, 0.3)',
+    lineWidth: 1
   }
 }
